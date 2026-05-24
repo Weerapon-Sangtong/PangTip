d15af44bfe88851125e7153c5426c79175f42a51
WEERAPON SANGTONG <weerapon.sa@rmuti.ac.th>
2026-05-24 23:16:54 +0700

    Delete WWW directory

--- WWW/config/check.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    session_start();
-    require_once 'db.php';
-    if(!isset($_SESSION['admin_login']) && !isset($_SESSION['user_login'])){
-        $_SESSION['error'] = 'กรุณาเข้าสู่ระบบ';
-        header('location: signin.php');
-    }
-?>
